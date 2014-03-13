8eab4984c8c228cb656250060dd05497cf7a1ba5
RichAE91 <RichAE_91@hotmail.com>
2014-03-13 18:39:37 +0000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -43,7 +43,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 2//4;
+    var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -123,7 +123,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 4096 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 4096) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
